4e64a5860290d8558a3c2a063b1732a3a59a95c2
BenKamotho <105359629+BernardKamotho@users.noreply.github.com>
2025-06-17 11:20:44 +0300

    first ap

---
+++ app.js
@@ -0,0 +1,18 @@
+// import the chalk dependecy
+const chalk = require("chalk")
+
+
+console.log("My first Node application")
+
+console.log(chalk.red("Hello there"))
+console.log(chalk.blue("This is another line of code..."))
+console.log(chalk.bgYellow("The servcer is running...."))
+
+
+// how create a node application ~ npm init -y
+// how to install packages either globally or locally
+// How to configure the start script inside of package.json file.
+
+
+// Assignment: Create a new node project that contains a function which is find the sum of three numbers.
+// Inside, install chalk package an use it on the output
