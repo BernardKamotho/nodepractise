feea59233c4d5c5424402d2f15744fa7ece25ba9
BenKamotho <105359629+BernardKamotho@users.noreply.github.com>
2025-06-23 11:10:04 +0300

    added webser and buffer files

--- streams/stream2.js
+++ streams/stream2.js
@@ -3,13 +3,12 @@
 const fs = require("fs")
 
 // highWaterMark object is used to divide the buffered data into some buffer bytes
-const readStream = fs.createReadStream("input.txt", {highWaterMark : 10})
+const readStream = fs.createReadStream("input.txt", {highWaterMark : 20})
 
 // simulate how the data get transfered from the input.txt inform of bytes
 readStream.on("data", (chunk) =>{
     console.log("New packets of data received: " + chunk.toString())
 });
-
 
 // below event shall be triggered when the entire file data has been read
 readStream.on("end", ()=>{
